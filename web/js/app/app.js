3b00b65bf2143a2a3a2ed819203dccc7bdda1200
stephen williams <stephen.ray.williams@gmail.com>
2015-11-22 00:36:58 -0600

    Adapting input form to my use case.

--- web/js/app/app.js
+++ web/js/app/app.js
@@ -93,6 +93,19 @@
                             controller: 'inputController'
                         });
                 // configure html5 to get links working on jsfiddle
-                $locationProvider.html5Mode({enabled:true});
+                $locationProvider.html5Mode({enabled: true});
             });
 })(window.angular);
+
+var thumbsUp = element(by.css('span.glyphicon-thumbs-up'));
+var thumbsDown = element(by.css('span.glyphicon-thumbs-down'));
+
+it('should check ng-show / ng-hide', function() {
+  expect(thumbsUp.isDisplayed()).toBeFalsy();
+  expect(thumbsDown.isDisplayed()).toBeTruthy();
+
+  element(by.model('denomradio')).click();
+
+  expect(thumbsUp.isDisplayed()).toBeTruthy();
+  expect(thumbsDown.isDisplayed()).toBeFalsy();
+});
